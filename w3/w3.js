fadf32f84d29070b2d1837d364d7235159227c83
Shuvo Riaujul <shuvoriaujul123@gmail.com>
2025-03-05 23:57:33 +0600

    done js problem

---
+++ w3/w3.js
@@ -0,0 +1,16 @@
+// Problem 1: Get the Next Integer
+function getNextInteger(idLastDigit) {
+  return idLastDigit + 1;
+}
+
+// my last id last digit 157
+let lastDigit = 7; // Replace this with your ID's last digit
+console.log("Next integer:", getNextInteger(lastDigit));
+
+
+
+// 2. create a loop that prints all the even numbers from 1-100.
+for (let i = 2; i <= 100; i += 2) {
+  console.log(i);
+}
+
